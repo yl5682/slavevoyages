76b4076fffbe53b940a4a498f4a688ef6b297519
Yang <yang@truman.edu>
2017-05-13 22:21:05 -0400

    change route

--- app/router.js
+++ app/router.js
@@ -8,7 +8,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('index',  {path: '/'});
-  this.route('database');
+  this.route('databases');
   this.route('gallery');
 });
 
